79e8e40f6f58da012e193847ce36e98696f0e04a
daria1818 <dashka_kik@mail.ru>
2024-07-11 15:03:38 +0300

    удалила папку для отладки

--- local/components/pwd/__crm.order.details/templates/.default/lang/ua/template.php
+++
@@ -1,8 +0,0 @@
-<?
-$MESS["CRM_ORDER_CANCEL_TITLE"] = "Скасування замовлення";
-$MESS["CRM_ORDER_CANCEL_DIALOG_CONTENT"] = "Ви впевнені, що хочете скасувати це замовлення?";
-$MESS["CRM_ORDER_RESTORE_TITLE"] = "Відновлення замовлення";
-$MESS["CRM_ORDER_RESTORE_DIALOG_CONTENT"] = "Ви впевнені, що хочете відновити це скасоване замовлення?";
-$MESS["CRM_ORDER_CANCEL_DIALOG_COMMENT_TITLE"] = "Коментар";
-$MESS["CRM_ORDER_DETAIL_HISTORY_STUB"] = "Прямо зараз ви створюєте замовлення...";
-?>
